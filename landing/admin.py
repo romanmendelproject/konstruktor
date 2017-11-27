a8e2c4c47ae60b1804c60f3edc0a71ff4c70cb44
unknown <romanmendel777@gmail.com>
2017-11-27 21:03:10 +0300

    '27.11.2017'

--- landing/admin.py
+++ landing/admin.py
@@ -1,4 +1,22 @@
 from django.contrib import admin
 from .models import *
 
+class nametestAdmin (admin.ModelAdmin):
+    # list_display = ["name", "email"]
+    list_display = [field.name for field in nametest._meta.fields]
+#    list_filter = ['name',]
+#    search_fields = ['name', 'email']
 
+   # fields = ["name"]
+
+    # exclude = ["email"]
+	# inlines = [FieldMappingInline]
+	# fields = []
+    # #exclude = ["type"]
+	# #list_filter = ('report_data',)
+	# search_fields = ['category', 'subCategory', 'suggestKeyword']
+
+    class Meta:
+        model = nametest
+
+admin.site.register(nametest, nametestAdmin)
--- landing/views.py
+++ landing/views.py
@@ -3,10 +3,13 @@ from .forms import nametestForm
 from .models import *
 
 def landing(request):
+
+    return render(request, 'landing/landing.html', locals())
+
+def home(request):
     name = "CodingMedved"
     current_day = "03.01.2017"
     formN = nametestForm(request.POST or None)
-
     if request.method == "POST" and formN.is_valid():
         print (request.POST)
         print (formN.cleaned_data)
@@ -14,9 +17,4 @@ def landing(request):
         print (data["name"])
 
         new_form = formN.save()
-
-    return render(request, 'landing/landing.html', locals())
-
-def home(request):
-
     return render(request, 'landing/index.html', locals())
